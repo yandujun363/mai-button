0e242f912d0ef0cab0fcd0d2f60172a4f145581b
yandujin363 <yangdujun363@outlook.com>
2026-01-06 13:45:09 +0800

    删除用不上的CDN节点配置

--- src/config/cdns.js
+++ src/config/cdns.js
@@ -7,13 +7,6 @@ export const CDN_CONFIGS = [
         description: '主要CDN源，通过Cloudflare加载',
         priority: 1
     },
-    // {
-    //     id: 'cdn2',
-    //     name: '杭州市 阿里云(善恶如辨OvO的服务器)',
-    //     url: 'https://auido.shanerubian.online/auido/3546775765912341/',
-    //     description: '借善恶如辨OvO的服务器实现的音频源',
-    //     priority: 2
-    // },
     {
         id: 'cdn3',
         name: 'NASCDN',
