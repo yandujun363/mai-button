19f3cd39913d27f1d0d179cd1bfea220106a5056
yandujin363 <yangdujun363@outlook.com>
2026-09-03 15:56:46 +0800

    想了想还是塞个本地文件，防止哪天我手贱删了CDN文件导致爆炸。

--- src/config/cdns.js
+++ src/config/cdns.js
@@ -1,6 +1,13 @@
 // src/config/cdns.js
 export const CDN_CONFIGS = [
     {
+        id: 'local',
+        name: '本地文件',
+        url: '/public/voices/3546775765912341/',
+        description: '使用本地文件夹中的音频文件',
+        priority: 0
+    },
+    {
         id: 'cdn1',
         name: '主CDN(Cloudflare)',
         url: 'https://cdn.yangdujun.top/auido/3546775765912341/',
